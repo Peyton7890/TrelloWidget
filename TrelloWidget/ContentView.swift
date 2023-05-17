e3d244043572a196e31f449e60bbc45cc04948f8
Peyton Martin <peytonmartin7890@gmail.com>
2023-05-17 16:03:48 -0400

    Starter code generated by chatGPT

--- TrelloWidget/ContentView.swift
+++ TrelloWidget/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var cards: [Card] = []
+    @State private var newCardName = ""
+    
+    private let trelloManager = TrelloManager()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            List(cards) { card in
+                Text(card.name)
+            }
+            
+            HStack {
+                TextField("New Card Name", text: $newCardName)
+                Button(action: addNewCard) {
+                    Text("Add Card")
+                }
+            }
         }
-        .padding()
+        .onAppear {
+            fetchCards()
+        }
+    }
+    
+    private func fetchCards() {
+        trelloManager.fetchCards { fetchedCards in
+            DispatchQueue.main.async {
+                cards = fetchedCards
+            }
+        }
+    }
+    
+    private func addNewCard() {
+        trelloManager.addCard(name: newCardName)
+        newCardName = ""
+        fetchCards()
+    }
+    
+    private func removeCard(at offsets: IndexSet) {
+        offsets.forEach { index in
+            let card = cards[index]
+            trelloManager.removeCard(cardId: card.id)
+        }
+        fetchCards()
     }
 }
 
@@ -24,3 +59,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
--- TrelloWidget/TrelloWidgetApp.swift
+++ TrelloWidget/TrelloWidgetApp.swift
@@ -15,3 +15,49 @@ struct TrelloWidgetApp: App {
         }
     }
 }
+
+struct Card: Identifiable, Decodable {
+    let id: String
+    let name: String
+}
+
+class TrelloManager {
+    private let apiKey = "YOUR_TRELLO_API_KEY"
+    private let apiToken = "YOUR_TRELLO_API_TOKEN"
+    private let boardId = "YOUR_TRELLO_BOARD_ID"
+    
+    func fetchCards(completion: @escaping ([Card]) -> Void) {
+        // Implement API call to fetch cards from Trello
+        // Use Alamofire, URLSession, or other networking libraries
+        
+        // Example implementation using URLSession
+        guard let url = URL(string: "https://api.trello.com/1/boards/\(boardId)/cards?key=\(apiKey)&token=\(apiToken)") else {
+            completion([])
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, _, _) in
+            guard let data = data else {
+                completion([])
+                return
+            }
+            
+            // Parse the JSON response and create an array of Card objects
+            if let cards = try? JSONDecoder().decode([Card].self, from: data) {
+                completion(cards)
+            } else {
+                completion([])
+            }
+        }.resume()
+    }
+    
+    func addCard(name: String) {
+        // Implement API call to add a card to Trello
+        // Use Alamofire, URLSession, or other networking libraries
+    }
+    
+    func removeCard(cardId: String) {
+        // Implement API call to remove a card from Trello
+        // Use Alamofire, URLSession, or other networking libraries
+    }
+}

993f0591d5e429121c24eef77fbd25c17424d43b
Peyton Martin <peytonmartin7890@gmail.com>
2023-05-17 15:47:04 -0400

    Initial Commit

---
+++ TrelloWidget/TrelloWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrelloWidgetApp.swift
+//  TrelloWidget
+//
+//  Created by Peyton Martin on 5/17/23.
+//
+
+import SwiftUI
+
+@main
+struct TrelloWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
